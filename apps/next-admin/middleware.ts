8a64fbf2d2a9c3c49fa721d6358c8ef7f836caee
mobssie <mobssie.jung@gmail.com>
2024-04-23 15:36:57 +0900

    feat: middleware 추가

---
+++ apps/next-admin/middleware.ts
@@ -0,0 +1,8 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+
+export async function middleware(request: NextRequest) {
+
+  return NextResponse.next()
+}
